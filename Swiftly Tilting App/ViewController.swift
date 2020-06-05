1aa8cbc43be296bc071be1cad3d7ccf300afd976
Avery Yen <ayen@pivotal.io>
2020-06-05 09:59:31 -0400

    Initial Commit

---
+++ Swiftly Tilting App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swiftly Tilting App
+//
+//  Created by pivotal on 6/5/20.
+//  Copyright © 2020 pivotal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Swiftly Tilting AppTests/Swiftly_Tilting_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  Swiftly_Tilting_AppTests.swift
+//  Swiftly Tilting AppTests
+//
+//  Created by pivotal on 6/5/20.
+//  Copyright © 2020 pivotal. All rights reserved.
+//
+
+import XCTest
+@testable import Swiftly_Tilting_App
+
+class Swiftly_Tilting_AppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
